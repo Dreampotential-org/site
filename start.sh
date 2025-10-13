35fa50285cdc5ed6f61a9358d2e411d8a3c827c3
root <root@ip-172-31-19-250.us-east-2.compute.internal>
2025-10-13 19:30:41 +0000

    update

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export COMPOSE_HTTP_TIMEOUT=20000
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml build
+docker compose -f docker-compose.yml up -d
 
 exec "$@"
